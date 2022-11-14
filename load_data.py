a2f0ae9e5f3ea9084f715b57edbe2f83dde19b11
huydao <huydao@tdt.asia>
2022-11-14 14:30:07 +0700

    add

--- load_data.py
+++ load_data.py
@@ -1,16 +1,10 @@
 from PIL import Image
 from torch.utils import data
-from torchvision import transforms
-
-
+from transform import transformer
 class Data(data.Dataset):
-    def __init__(self, db_dir, is_train):
-        self.is_train = is_train
-        self.file_list, self.label = self.get_file_list(db_dir)
-        if self.is_train:
-            self.transforms = transforms.Compose([transforms.ToTensor()])
-        else:
-            self.transforms = transforms.Compose([transforms.ToTensor()])
+    def __init__(self,label_list,transforms):
+        self.file_list, self.label = self.get_file_list(label_list)
+        self.transforms = transforms
 
     def __getitem__(self, index):
         img = self.transforms(Image.open(self.file_list[index]).convert("RGB"))
@@ -21,20 +15,13 @@ class Data(data.Dataset):
     def __len__(self):
         return len(self.file_list)
 
-    def get_file_list(self, db_dir):
+    def get_file_list(self,label_lists):
         file_list = []
         label_list = []
-        for file in open(db_dir + "/file_list.txt", "r"):
+        for file in label_lists:
             file_info = file.strip("\n").split(" ")
             file_name = file_info[0]
             label = file_info[1]
             file_list.append(file_name)
             label_list.append(int(label))
         return file_list, label_list
-
-
-if __name__ == "__main__":
-    data = Data("G:/zalo_challenge/liveness_face/datasets/crop/scale_1.0/", True)
-    img, label = data[0]
-    print(img.shape)
-    print(label)
---
+++ transform.py
@@ -0,0 +1,13 @@
+from torchvision import transforms as trans
+
+train_transform = trans.Compose(
+    [
+        trans.RandomResizedCrop(size=(128, 128), scale=(0.9, 1.1)),
+        trans.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+        trans.RandomRotation(10),
+        trans.RandomHorizontalFlip(),
+        trans.ToTensor(),
+    ]
+)
+test_transform = trans.Compose([trans.ToTensor()])
+transformer = {"train": train_transform, "test": test_transform}
