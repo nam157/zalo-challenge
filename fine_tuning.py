485ed9f59ff39cc9294418ee76ef0a2796eeee81
DLH06 <daolehuy99@gmail.com>
2022-11-15 11:02:48 +0700

    fine_tuning silent anti-spoofing

---
+++ fine_tuning.py
@@ -0,0 +1,161 @@
+import os
+from typing import Dict
+
+import torch
+import torch.nn.functional as F
+from sklearn.model_selection import train_test_split
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from load_data import Dataset,transformer
+from models.MiniFASNet import MiniFASNetV1, MiniFASNetV1SE, MiniFASNetV2, MiniFASNetV2SE
+from utils import get_kernel,parse_model_name
+
+MODEL_MAPPING = {
+    "MiniFASNetV1": MiniFASNetV1,
+    "MiniFASNetV2": MiniFASNetV2,
+    "MiniFASNetV1SE": MiniFASNetV1SE,
+    "MiniFASNetV2SE": MiniFASNetV2SE,
+}
+SCALE = 2.7
+HEIGHT = 80
+WIDTH = 80
+MODEL_TYPE = "MiniFASNetV2"
+
+class anti_spoofing:
+
+    def __init__(
+        self,
+        label_path: str,
+        params: Dict,
+        model_path: str = None,
+        pre_trained: bool = False,
+    ) -> None:
+        self.device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
+        if pre_trained:
+            model_name = os.path.basename(model_path)
+            h_input, w_input, model_type, _ = parse_model_name(model_name)
+            self.kernel_size = get_kernel(
+                h_input,
+                w_input,
+            )
+            self.model = MODEL_MAPPING[model_type](conv6_kernel=self.kernel_size).to(self.device)
+            self.model = torch.nn.DataParallel(self.model)
+            state_dict = torch.load(model_path, map_location=self.device)
+            self.model.load_state_dict(state_dict)
+        else:
+            self.kernel_size = get_kernel(80, 80)
+            self.model = MODEL_MAPPING[MODEL_TYPE](conv6_kernel=self.kernel_size).to(self.device)
+        
+        self.train_data_loader, self.val_data_loader = self._init_data(
+            label_path, params)
+        (
+            self.optimizer,
+            self.scheduler,
+            self.cls_criterion,
+        ) = self._init_params()
+
+
+    def _init_data(self, label_path: str, params: Dict):
+        fh = open(label_path, "r")
+        labels = fh.readlines()
+        label_train, label_valid = train_test_split(labels,
+                                                    test_size=0.2,
+                                                    random_state=121)
+        kernel_size = get_kernel(80, 80)
+        ft_h, ft_w = 2 * kernel_size[0], 2 * kernel_size[1]
+        data_train = Dataset(
+            label_list=label_train,
+            transforms=transformer["train"],
+        
+        )
+        data_val = Dataset(
+            label_list=label_valid,
+            transforms=transformer["test"],
+         
+        )
+
+        train_data_loader = DataLoader(data_train, **params)
+        val_data_loader = DataLoader(data_val, **params)
+        return train_data_loader, val_data_loader
+
+    def _init_params(self):
+        optimizer = torch.optim.SGD(self.model.parameters(),
+                                    lr=0.001,
+                                    momentum=0.9)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                               T_max=10000,
+                                                               eta_min=1e-7)
+        cls_criterion = CrossEntropyLoss()
+       
+
+        return optimizer, scheduler, cls_criterion
+
+    def _train_one_epoch(self, device):
+        self.model.train()
+        loss_sum = 0
+        for img, label in tqdm(self.train_data_loader,
+                                            desc="Training"):
+            img = img.to(device)
+            label = label.to(device)
+            self.optimizer.zero_grad()
+            out = self.model(img)
+            train_loss = self.cls_criterion(out, label)
+
+            train_loss.backward()
+            self.optimizer.step()
+
+            loss_sum += train_loss
+        return loss_sum / len(self.train_data_loader)
+
+    def _valid_one_epoch(self, device):
+        self.model.eval()
+        loss_sum = 0
+        for img, label in tqdm(self.val_data_loader,
+                                            desc="Validation"):
+            with torch.no_grad():
+                img = img.to(device)
+                label = label.to(device)
+
+                out = self.model(img)
+                valid_loss = self.cls_criterion(out, label)
+
+                loss_sum += valid_loss
+        return loss_sum / len(self.val_data_loader)
+
+    def fit(self, epochs):
+
+        for epoch in range(epochs):
+            loss_train = self._train_one_epoch(device=self.device)
+            print(f"Epochs: {epoch}/{epochs}--- Loss-Train: {loss_train}")
+            if epoch % 5 == 0:
+                loss_val = self._valid_one_epoch(device=self.device)
+                print(
+                    f"Epochs: {epoch}/{epochs}--- Loss-Train: {loss_train} ----- Loss-Valid: {loss_val}"
+                )
+                save_dir = "ckpt/anti_spoofing/"
+                os.makedirs(save_dir, exist_ok=True)
+                torch.save(
+                    self.model.state_dict(),
+                    save_dir + f"{SCALE}_{HEIGHT}x{WIDTH}_{MODEL_TYPE}.pth",
+                )
+        self.scheduler.step()
+
+
+if __name__ == "__main__":
+    params = {
+        "batch_size": 16,
+        "shuffle": True,
+        "num_workers": 8,
+        "pin_memory": True
+    }
+    deep_fake = anti_spoofing(
+        label_path=
+        "/home/ai/challenge/zalo-challenge/datasets/img_crops/scale_2.7/file_label.txt",
+        params=params,
+        model_path=
+        "/home/ai/challenge/test/anti-spoofing/pre-trained/anti_spoof_models/2.7_80x80_MiniFASNetV2.pth",
+        pre_trained=True,
+    )
+
+    deep_fake.fit(epochs=100)
---
+++ load_data.py
@@ -0,0 +1,45 @@
+from PIL import Image
+from torch.utils import data
+from torchvision import transforms as trans
+
+train_transform = trans.Compose([
+    # trans.ToPILImage(),
+    trans.RandomResizedCrop(size=(80, 80), scale=(0.9, 1.1)),
+    trans.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1),
+    trans.RandomRotation(10),
+    trans.RandomHorizontalFlip(),
+    trans.ToTensor(),
+])
+test_transform = trans.Compose([trans.ToTensor(),trans.Resize(size=(80,80))])
+transformer = {"train": train_transform, "test": test_transform}
+
+
+class Dataset(data.Dataset):
+
+    def __init__(self, label_list, transforms):
+        self.file_list, self.label = self.get_file_list(label_list)
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        if self.transforms:
+            img = self.transforms(
+                Image.open(self.file_list[index]).convert("RGB"))
+        else:
+            img = Image.open(self.file_list[index]).convert("RGB")
+
+        label = self.label[index]
+        return img, label
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def get_file_list(self, label_lists):
+        file_list = []
+        label_list = []
+        for file in label_lists:
+            file_info = file.strip("\n").split(" ")
+            file_name = file_info[0]
+            label = file_info[1]
+            file_list.append(file_name)
+            label_list.append(int(label))
+        return file_list, label_list
