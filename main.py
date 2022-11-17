7419ffcb604aea726eed61f294c4598eb9dce263
Dao Le Huy <huydao@tdt.asia>
2022-11-17 14:49:37 +0700

    softmax

--- main.py
+++ main.py
@@ -51,6 +51,7 @@ if __name__ == '__main__':
             label = label.to(DEVICE)
 
             pred = model(img)
+            pred = torch.nn.functional.softmax(pred)
             loss = criterion(pred, label)
 
             optimizer.zero_grad()
@@ -68,6 +69,7 @@ if __name__ == '__main__':
                 label = label.to(DEVICE)
 
                 pred = model(img)
+                pred = torch.nn.functional.softmax(pred)
                 loss = criterion(pred, label)
 
                 val_loss += loss.item()
